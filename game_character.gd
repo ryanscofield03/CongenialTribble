8f53991def019b5822813e22f3fbc412c79c2823
Ryan Scofield <rsc104@uclive.ac.nz>
2024-11-22 18:23:18 +1300

    Fixed animations and walking (changing direction with movement)

--- game_character.gd
+++ game_character.gd
@@ -2,9 +2,9 @@ extends Node3D
 
 @export var pickup_anim_running: bool = false
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	print(anim_name)
-	print("HERE")
-	
-	if anim_name == "pickup":
+func _ready() -> void:
+	$AnimationPlayer.animation_finished.connect(_handle_animation_finished)
+
+func _handle_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Pickup":
 		pickup_anim_running = false
--- player.gd
+++ player.gd
@@ -2,24 +2,24 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const ROTATION_SPEED = 5.0
 
 @onready var animation_player: AnimationPlayer = $PlayerCollision/PlayerMesh/AnimationPlayer
 var current_animation: String = "Idle"
+
+var last_direction: Vector3 = Vector3.DOWN
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")	
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		last_direction = direction
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
@@ -29,10 +29,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	if velocity.length() > 0.1:
-		var facing_direction = velocity.normalized()
-		var facing_angle = atan2(facing_direction.x, facing_direction.z)
-		$RayCastPickup.rotation = Vector3(0, facing_angle, 0)
-		
+		#var facing_direction = velocity.normalized()
+		#var facing_angle = atan2(facing_direction.x, facing_direction.z)
+		#$RayCastPickup.rotation = Vector3(0, facing_angle, 0)	
 		current_animation = "Walk"
 	else:
 		current_animation = "Idle"
@@ -42,8 +41,8 @@ func _physics_process(delta: float) -> void:
 		if $RayCastPickup.is_colliding():
 			print("Handle Pickup")
 	
+	$PlayerCollision/PlayerMesh.rotation.y = lerp_angle($PlayerCollision/PlayerMesh.rotation.y, atan2(last_direction.x, last_direction.z), delta * ROTATION_SPEED)
 	handle_animation()
-	
 	
 func handle_animation():	
 	if $PlayerCollision/PlayerMesh.pickup_anim_running == false:	
